821009a33ee09029f5d3741d11bf97f85087559f
Lucas Flauzino <27flauzino@gmail.com>
2024-04-17 17:26:27 -0300

    views para pagamento de cardcredito, boleto e pix integradas, restam melhorias

--- pagamento/views.py
+++ pagamento/views.py
@@ -1,8 +1,12 @@
 import json
 import requests
 
-from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse
+import pytz
+
+from datetime import timedelta, datetime
+
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
 
 from pedido.models import Pedido
 from perfil.models import Perfil
@@ -11,8 +15,9 @@ from perfil.models import Perfil
 def get_public_key():
     url = 'https://sandbox.api.pagseguro.com/public-keys/'
     headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'E0A4D184FD684D5D9036F8DE1C7DBE34'
+        "accept": "application/json",
+        "Authorization": "Bearer E0A4D184FD684D5D9036F8DE1C7DBE34",
+        "content-type": "application/json"
     }
     body = json.dumps({
         "type": "card"
@@ -23,9 +28,14 @@ def get_public_key():
 
 def inicio(request, pk):
     pedido = get_object_or_404(Pedido, pk=pk)
+    # Se o parâmetro não estiver presente, o método de pagamento
+    # padrão será 'Cartao'
+    metodo_pagamento = request.GET.get('metodo_pagamento', 'Cartao')
+
     data = {
         'public_key': get_public_key(),
-        'pedido': pedido
+        'pedido': pedido,
+        'pagamento': metodo_pagamento
     }
     return render(
         request,
@@ -43,7 +53,13 @@ def cartao_cred(request, pk):
     # como item_pedido sera uma lista
     # é feito um for para passar os valores para dentro de variaveis
     for item in item_pedido:
-        nome_produto = item.produto
+        # conferindo se o nome do item é menor que 100 chars
+        # pois é o limite do pagseguro
+        if len(item.produto) > 100:
+            nome_produto = item.produto[:99]
+        else:
+            nome_produto = item.produto
+
         qtd = item.quantidade
         # checando se tem preço promocional no item
         if item.preco_promocional:
@@ -56,10 +72,7 @@ def cartao_cred(request, pk):
 
     # Aqui obtem os dados do formulário do request.POST
     # do cartão de credito
-    card_number = request.POST.get('cardNumber')
     card_holder = request.POST.get('cardHolder')
-    card_month = request.POST.get('cardMonth')
-    card_year = request.POST.get('cardYear')
     card_cvv = request.POST.get('cardCvv')
     encrypted_card = request.POST.get('encriptedCard')
 
@@ -73,10 +86,10 @@ def cartao_cred(request, pk):
     cliente_cpf = cliente.cpf
 
     # iniciando a integração com a API do pagseguro
-    url = 'https://sandbox.api.pagseguro.com/orders'
+    url = 'https://sandbox.api.pagseguro.com/orders/'
     headers = {
         'Content-Type': 'application/json',
-        'Authorization': 'E0A4D184FD684D5D9036F8DE1C7DBE34'
+        'Authorization': 'Bearer E0A4D184FD684D5D9036F8DE1C7DBE34'
     }
     # corpo de envio para API do pagseguro
     body = json.dumps({
@@ -85,14 +98,6 @@ def cartao_cred(request, pk):
             "name": cliente_nome,
             "email": cliente_email,
             "tax_id": cliente_cpf,
-            "phones": [
-                {
-                    "country": "55",
-                    "area": "11",
-                    "number": "999999999",
-                    "type": "MOBILE"
-                }
-            ]
         },
         "items": [
             {
@@ -163,44 +168,80 @@ def cartao_cred(request, pk):
 
 
 def boleto(request, pk):
+    # pegando a data de hoje para calcular vencimento do boleto.
+    hoje = timezone.now().date()
+    vencimento = hoje + timedelta(days=5)
+    # pegando o pedido
+    pedido = get_object_or_404(Pedido, pk=pk)
+    # pegando item pedido
+    item_pedido = pedido.itempedido_set.all()
+
+    # como item_pedido sera uma lista
+    # é feito um for para passar os valores para dentro de variaveis
+    for item in item_pedido:
+        # conferindo se o nome do item é menor que 100 chars
+        # pois é o limite do pagseguro
+        if len(item.produto) > 100:
+            nome_produto = item.produto[:99]
+        else:
+            nome_produto = item.produto
+
+        qtd = item.quantidade
+        # checando se tem preço promocional no item
+        if item.preco_promocional:
+            valor_item = item.preco_promocional
+        else:
+            valor_item = item.preco
+
+    # pegando o cliente
+    cliente = Perfil.objects.get(user=request.user)
+
+    # separando os dados do cliente
+    if cliente.user.first_name:
+        cliente_nome = cliente.user.first_name
+    else:
+        cliente_nome = cliente.user.username
+
+    cliente_email = cliente.user.email
+    cliente_cpf = cliente.cpf
+
+    # pegando valor total para calcular desconto com boleto.
+    valor_total = pedido.total * 100
+    # desconto de 10% a vista no boleto.
+    disconto_boleto_total = valor_total - (valor_total * 0.1)
+
     url = 'https://sandbox.api.pagseguro.com/orders'
     headers = {
         'Content-Type': 'application/json',
-        'Authorization': 'E0A4D184FD684D5D9036F8DE1C7DBE34'
+        'Accept': 'application/json',
+        'Authorization': 'Bearer E0A4D184FD684D5D9036F8DE1C7DBE34'
     }
+
     body = json.dumps({
-        "reference_id": "ex-00001",
+        "reference_id": str(pedido.id),
         "customer": {
-            "name": "Jose da Silva",
-            "email": "email@test.com",
-            "tax_id": "12345679891",
-            "phones": [
-                {
-                    "country": "55",
-                    "area": "11",
-                    "number": "999999999",
-                    "type": "MOBILE"
-                }
-            ]
+            "name": cliente_nome,
+            "email": cliente_email,
+            "tax_id": cliente_cpf,
         },
         "items": [
             {
-                "reference_id": "referencia do item",
-                "name": "nome do item",
-                "quantity": 1,
-                "unit_amount": 5000
+                "reference_id": str(pedido.id),
+                "name": nome_produto,
+                "quantity": qtd,
+                "unit_amount": int(valor_item * 100)
             }
         ],
         "shipping": {
             "address": {
-                "street": "Avenida Brigadeiro Faria Lima",
-                "number": "1384",
-                "complement": "apto 12",
-                "locality": "Pinheiros",
-                "city": "São Paulo",
-                "region_code": "SP",
+                "street": cliente.endereco,
+                "number": cliente.numero,
+                "complement": cliente.complemento,
+                "locality": cliente.bairro,
+                "city": cliente.cidade,
+                "region_code": cliente.estado,
                 "country": "BRA",
-                "postal_code": "01452002"
+                "postal_code": cliente.cep
             }
         },
         "notification_urls": [
@@ -208,33 +249,33 @@ def boleto(request, pk):
         ],
         "charges": [
             {
-                "reference_id": "referencia da cobranca",
+                "reference_id": str(pedido.id),
                 "description": "descricao da cobranca",
                 "amount": {
-                    "value": 500,
+                    "value": int(disconto_boleto_total),
                     "currency": "BRL"
                 },
                 "payment_method": {
                     "type": "BOLETO",
                     "boleto": {
-                        "due_date": "2024-04-18",
+                        "due_date": str(vencimento),
                         "instruction_lines": {
                             "line_1": "Pagamento processado para DESC Fatura",
                             "line_2": "Via PagSeguro"
                         },
                         "holder": {
-                            "name": "Jose da Silva",
-                            "tax_id": "12345679891",
-                            "email": "jose@email.com",
+                            "name": cliente_nome,
+                            "tax_id": cliente_cpf,
+                            "email": cliente_email,
                             "address": {
                                 "country": "Brasil",
-                                "region": "São Paulo",
-                                "region_code": "SP",
-                                "city": "Sao Paulo",
-                                "postal_code": "01452002",
-                                "street": "Avenida Brigadeiro Faria Lima",
-                                "number": "1384",
-                                "locality": "Pinheiros"
+                                "region": cliente.estado,
+                                "region_code": cliente.estado,
+                                "city": cliente.cidade,
+                                "postal_code": cliente.cep,
+                                "street": cliente.endereco,
+                                "number": cliente.numero,
+                                "locality": cliente.bairro
                             }
                         }
                     }
@@ -243,56 +284,147 @@ def boleto(request, pk):
         ]
     })
     reqs = requests.post(url, headers=headers, data=body)
-    return HttpResponse(reqs)
-    return redirect(reqs.json()['links'][0]['href'])  # verificar depois
+    if reqs.status_code == 201:
+        pedido.status = 'Aprovado'
+        pedido.save()
+
+        ordem_id = reqs.json()['id']
+
+        link_boleto_pdf = None
+
+        for charge in reqs.json()['charges']:
+            if 'links' in charge:
+                for link in charge['links']:
+                    if link['media'] == 'application/pdf':
+                        link_boleto_pdf = link['href']
+                        break
+                if link_boleto_pdf:
+                    break
+
+        # Verificar se o link foi encontrado
+        if link_boleto_pdf:
+            # Redirecionar para o link do PDF do boleto
+
+            contexto = {
+                'ordem_id': ordem_id,
+                'pedido': pedido,
+                'pagamento': 'BOLETO',
+                'link_boleto': link_boleto_pdf
+            }
+
+            return render(
+                request,
+                'pagamento/status_compra.html',
+                contexto
+            )
+
+    else:
+        # Caso ocorra algum problema com o pagamento
+        contexto = {
+            'erro': 'Não foi possivel gerar o boleto'
+        }
+        return render(
+            request,
+            'pagamento/error_pagamento.html',
+            contexto
+        )
 
 
 def pix(request, pk):
+    # pegando a data de hoje para calcular vencimento do qrcode.
+    hoje = datetime.now()
+
+    # Adicionando 5 dias ao momento atual
+    vencimento = hoje + timedelta(days=5)
+
+    # Adicionando o fuso horário -03:00
+    # -180 minutos é equivalente a -03:00
+    fuso_horario = pytz.FixedOffset(-180)
+    vencimento_com_fuso = vencimento.astimezone(fuso_horario)
+
+    # Formatando a data e hora no formato ISO-8601
+    vencimento_formatado = vencimento_com_fuso.strftime('%Y-%m-%dT%H:%M:%S%z')
+
+    # Inserindo ':' para separar as horas dos minutos no fuso horário
+    vencimento_formatado = vencimento_formatado[:-2] + ':' + vencimento_formatado[-2:]  # noqa: E501
+
+    print(vencimento_formatado)
+    # pegando o pedido
+    pedido = get_object_or_404(Pedido, pk=pk)
+    # pegando item pedido
+    item_pedido = pedido.itempedido_set.all()
+
+    # como item_pedido sera uma lista
+    # é feito um for para passar os valores para dentro de variaveis
+    for item in item_pedido:
+        # conferindo se o nome do item é menor que 100 chars
+        # pois é o limite do pagseguro
+        if len(item.produto) > 100:
+            nome_produto = item.produto[:99]
+        else:
+            nome_produto = item.produto
+
+        qtd = item.quantidade
+        # checando se tem preço promocional no item
+        if item.preco_promocional:
+            valor_item = item.preco_promocional
+        else:
+            valor_item = item.preco
+
+    # pegando o cliente
+    cliente = Perfil.objects.get(user=request.user)
+
+    # separando os dados do cliente
+    if cliente.user.first_name:
+        cliente_nome = cliente.user.first_name
+    else:
+        cliente_nome = cliente.user.username
+
+    cliente_email = cliente.user.email
+    cliente_cpf = cliente.cpf
+
+    # pegando valor total para calcular desconto com boleto.
+    valor_total = pedido.total * 100
+    # desconto de 10% a vista no boleto.
+    disconto_pix_total = valor_total - (valor_total * 0.1)
+
     url = 'https://sandbox.api.pagseguro.com/orders'
     headers = {
         'Content-Type': 'application/json',
         'Authorization': 'E0A4D184FD684D5D9036F8DE1C7DBE34'
     }
     body = json.dumps({
-        "reference_id": "ex-00001",
+        "reference_id": str(pedido.id),
         "customer": {
-            "name": "Jose da Silva",
-            "email": "email@test.com",
-            "tax_id": "12345678909",
-            "phones": [
-                {
-                    "country": "55",
-                    "area": "11",
-                    "number": "999999999",
-                    "type": "MOBILE"
-                }
-            ]
+            "name": cliente_nome,
+            "email": cliente_email,
+            "tax_id": cliente_cpf,
         },
         "items": [
             {
-                "name": "nome do item",
-                "quantity": 1,
-                "unit_amount": 500
+                "name": nome_produto,
+                "quantity": qtd,
+                "unit_amount": int(valor_item)
             }
         ],
         "qr_codes": [
             {
                 "amount": {
-                    "value": 500
+                    "value": int(disconto_pix_total)
                 },
-                "expiration_date": "2024-07-29T20:15:59-03:00",
+                "expiration_date": str(vencimento_formatado),
             }
         ],
         "shipping": {
             "address": {
-                "street": "Avenida Brigadeiro Faria Lima",
-                "number": "1384",
-                "complement": "apto 12",
-                "locality": "Pinheiros",
-                "city": "São Paulo",
-                "region_code": "SP",
+                "street": cliente.endereco,
+                "number": cliente.numero,
+                "complement": cliente.complemento,
+                "locality": cliente.bairro,
+                "city": cliente.cidade,
+                "region_code": cliente.estado,
                 "country": "BRA",
-                "postal_code": "01452002"
+                "postal_code": cliente.cep
             }
         },
         "notification_urls": [
@@ -300,4 +432,23 @@ def pix(request, pk):
         ]
     })
     reqs = requests.post(url, headers=headers, data=body)
-    return redirect(reqs.json()['qr_codes'][0]['links'][0]['href'])
+    if reqs.status_code == 201:
+        pedido.status = 'Aprovado'
+        pedido.save()
+
+        ordem_id = reqs.json()['id']
+        qrcode = reqs.json()['qr_codes'][0]['links'][0]['href']
+
+        contexto = {
+            'ordem_id': ordem_id,
+            'pedido': pedido,
+            'pagamento': 'PIX',
+            'qrcode': qrcode
+        }
+        return render(
+            request,
+            'pagamento/status_compra.html',
+            contexto
+        )
+    else:
+        return render(request, 'pagamento/error_pagamento.html')
